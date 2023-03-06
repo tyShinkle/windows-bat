9b29b9fff890f5729f60e8ef4ac49a74e45dcd33
tyShinkle <tyshinkle@gmail.com>
2023-03-05 18:42:04 -0600

    prettier

--- create-shortcut.bat
+++ create-shortcut.bat
@@ -1,4 +1,5 @@
-@echo off 
-echo [InternetShortcut] >> "%USERPROFILE%\desktop\github.url"
-echo URL = "https://github.com/" >> "%USERPROFILE%\desktop\github.url"
+@echo off
+echo [InternetShortcut]>>"%USERPROFILE%\desktop\github.url"
+echo URL="https://github.com/">>"%USERPROFILE%\desktop\github.url"
+echo Your shortcut is made!
 pause
