5133f6195e6bdd317a0603ce255b14f301eab270
tyShinkle <tyshinkle@gmail.com>
2023-03-08 16:03:26 -0600

    create-shortcut update to enable parameter passing

--- create-shortcut.bat
+++ create-shortcut.bat
@@ -1,5 +1,31 @@
 @echo off
-echo [InternetShortcut]>>"%USERPROFILE%\desktop\github.url"
-echo URL="https://github.com/">>"%USERPROFILE%\desktop\github.url"
-echo Your shortcut is made!
+
+set URL=%1%
+set SHORTCUT_NAME="%USERPROFILE%\Desktop\%2%.url"
+ 
+if "%URL%"=="" ( 
+	echo URL not specified. 
+	exit /b 1
+)
+
+if exist %SHORTCUT_NAME% (
+	echo Shortcut with same name exists!
+	pause
+	exit /b 0
+)
+
+if "%SHORTCUT_NAME%"=="" (
+	set SHORTCUTNAME="%USERPROFILE%\Desktop\shortcut.url"
+)
+
+echo Creating internet shortcut to "%URL%" named %SHORTCUT_NAME%...
+echo [InternetShortcut] >> "%SHORTCUT_NAME%"
+echo URL = "%URL%" >> "%SHORTCUT_NAME%
+
+if %ERRORLEVEL% EQU 0 (
+	echo Shortcut created!!
+) else (
+	echo Failed to create shortcut! Error Level = %ERRORLEVEL%
+)
+
 pause
